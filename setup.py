b85bcf6b4d54a7010da0bb3ca1c3dfadba4f07e0
Luka Stojanovic <luka@magrathea.rs>
2014-06-11 23:40:02 +0200

    Fancy up setup.py
    Added `zip_safe`, `test_suite`, `licence` and `classifiers` fields

--- setup.py
+++ setup.py
@@ -17,5 +17,19 @@ setup(
     },
     install_requires=[
         'nose==1.3.0', 'docker-py==0.3.1', 'docopt==0.6.1', 'requests==2.2.1', 'networkx==1.9rc1', 'pep8==1.5.7'
+    ],
+    zip_safe=False,
+    test_suite='tests',
+    license='AGPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)'
     ]
 )
