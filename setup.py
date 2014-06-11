dae99d59d420612e5340cd8f9c5adab72ca0b85a
Luka Stojanovic <luka@magrathea.rs>
2014-06-11 15:14:17 +0200

    downgraded requests to 2.2.1 to match docker-py dependency

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
         'console_scripts': ['rabix = rabix.runtime.cli:main'],
     },
     install_requires=[
-        'nose==1.3.0', 'docker-py==0.3.1', 'docopt==0.6.1', 'requests==2.3.0', 'networkx==1.9rc1', 'pep8==1.5.7'
+        'nose==1.3.0', 'docker-py==0.3.1', 'docopt==0.6.1', 'requests==2.2.1', 'networkx==1.9rc1', 'pep8==1.5.7'
     ]
 )
