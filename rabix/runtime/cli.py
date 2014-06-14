44c83f9681d151ec2b3bf3cbfa300c3bb1a1e028
Nebojsa Tijanic <nebojsa.tijanic@sbgenomics.com>
2014-06-14 02:00:01 +0200

    Fixed CLI options.

--- rabix/runtime/cli.py
+++ rabix/runtime/cli.py
@@ -1,10 +1,9 @@
-from __future__ import print_function
-
 import os
 import sys
 import logging
 
 from docopt import docopt, DocoptExit
+
 from rabix import VERSION
 from rabix.common.util import rnd_name
 from rabix.runtime import from_url
@@ -13,49 +12,46 @@ from rabix.runtime.scheduler import SequentialScheduler, RunJob, InstallJob
 log = logging.getLogger(__name__)
 
 
-USAGE_TPL = """Usage:
-rabix run {run_args}
-rabix install <pipeline.json>
+USAGE = """
+Usage:
+  rabix run <file>
+  rabix install <file>
+  rabix -h | --help
+  rabix --version
 
 Options:
--h --help        Display this message.
---version        Print version to standard output and quit.
+  -h --help        Display this message.
+  --version        Print version to standard output and quit.
+"""
+
+RUN_TPL = """
+Usage: rabix run {pipeline} {arguments}
+
+Options:
 {options}
 """
-USAGE = USAGE_TPL.format(run_args='<pipeline.json>', options='')
 
 
 def make_pipeline_usage_string(pipeline, path):
-    """
-    Takes a pipeline path string and returns the usage string for it.
-    As this is the usage string for a given pipeline, <pipeline.json>
-    is replaced with the actual pipeline (for the 'run' cmd)
-    """
-    inputs = pipeline.get_inputs()
-
-    usage_str = " "
-    options = "\nInputs:\n"
-    for inp_id, inp_details in inputs.iteritems():
-        arg = "--" + inp_id + "=" + (inp_id + "_file").upper()
-        arg += "..." if inp_details["list"] else ""
-        usage_str += arg + " " if inp_details["required"] else "[" + arg + "] "
-        options += '{0: <40}'.format(arg) + inp_details["description"] + "\n"
-    docstring_args = {'run_args': path + usage_str, 'options': options}
-    return USAGE_TPL.replace("rabix install <pipeline.json>\n", "").format(**docstring_args)
+    usage_str, options = [], []
+    for inp_id, inp_details in pipeline.get_inputs().iteritems():
+        arg = '--%s=<%s_file>' % (inp_id, inp_id) + ('...' if inp_details['list'] else '')
+        usage_str.append(arg if inp_details['required'] else '[%s]' % arg)
+        options.append('{0: <40}{1}'.format(arg, inp_details.get('description', '')))
+    return RUN_TPL.format(pipeline=path, arguments=' '.join(usage_str), options='\n'.join(options))
 
 
 def before_task(task):
-    print('Running', task.task_id)
+    print 'Running', task.task_id
     sys.stdout.flush()
 
 
 def present_outputs(outputs):
     row_fmt = '{:<20}{:<80}{:>16}'
-    print('')
-    print(row_fmt.format('Output ID', 'File path', 'File size'))
+    print row_fmt.format('Output ID', 'File path', 'File size')
     for out_id, file_list in outputs.iteritems():
         for path in file_list:
-            print(row_fmt.format(out_id, path, str(os.path.getsize(path))))
+            print row_fmt.format(out_id, path, str(os.path.getsize(path)))
 
 
 def run():
@@ -81,14 +77,14 @@ def main():
     except DocoptExit:
         if len(sys.argv) > 3:
             return run()
-        print(USAGE)
+        print USAGE
         return
     if args["run"]:
-        pipeline_path = args['<pipeline.json>']
+        pipeline_path = args['<file>']
         pipeline = from_url(pipeline_path)
-        print(make_pipeline_usage_string(pipeline, pipeline_path))
+        print make_pipeline_usage_string(pipeline, pipeline_path)
     elif args["install"]:
-        install(from_url(args['<pipeline.json>']))
+        install(from_url(args['<file>']))
 
 
 if __name__ == '__main__':
