e4e7fafb35b47352f845d98a70a9cb308c73c17c
Milica Kadic <milica.kadic@sbgenomics.com>
2014-07-03 10:57:09 +0200

    adjust height on resize

--- rabix/registry/dev/app/scripts/directives/height.js
+++ rabix/registry/dev/app/scripts/directives/height.js
@@ -1,14 +1,36 @@
 'use strict';
 
 angular.module('registryApp')
-    .directive('height', [function () {
+    .directive('height', ['$window', '$timeout', function ($window, $timeout) {
         return {
             link: function(scope, element) {
 
-                var heading = 58;
-                var height = element[0].parentNode.offsetHeight - heading;
+                /**
+                 * Set the height of the content container
+                 */
+                var setHeight = function() {
 
-                element[0].style.height = height + 'px';
+                    var height = element[0].parentNode.offsetHeight - (scope.header + scope.footer);
+
+                    element[0].style.height = height + 'px';
+                };
+
+                var timeoutId = $timeout(function() {
+
+                    scope.header = element[0].parentNode.children[0].offsetHeight;
+                    scope.footer = angular.isDefined(element[0].parentNode.children[2]) ? element[0].parentNode.children[2].offsetHeight : 0;
+
+                    setHeight();
+                });
+
+                var lazySetHeight = _.debounce(setHeight, 150);
+
+                angular.element($window).on('resize', lazySetHeight);
+
+                element.on('$destroy', function() {
+                    angular.element($window).off('resize', lazySetHeight);
+                    $timeout.cancel(timeoutId);
+                });
 
             }
         };
