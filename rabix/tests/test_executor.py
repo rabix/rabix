1ea64d838c678a19104c34de8cd33a21927a2616
sinisa <sinisa.ivkovic@sbgenomics.com>
2014-10-21 14:25:50 +0200

    test paths fixed

--- rabix/tests/test_executor.py
+++ rabix/tests/test_executor.py
@@ -8,7 +8,6 @@ from nose.tools import nottest, raises
 from rabix.tests import mock_app_bad_repo, mock_app_good_repo
 from rabix.executors.cli import get_tool, main, dry_run_parse
 
-
 @nottest
 @raises(Exception)
 def test_provide_image_bad_repo():
@@ -19,7 +18,6 @@ def test_provide_image_bad_repo():
     docker_client = docker.Client(version='1.12')
     ensure_image(docker_client, imageId, uri)
 
-
 @nottest
 def test_provide_image_good_repo():
     uri = mock_app_good_repo["tool"]["requirements"]["environment"][
@@ -29,7 +27,7 @@ def test_provide_image_good_repo():
     docker_client = docker.Client(version='1.12')
     ensure_image(docker_client, imageId, uri)
 
-
+@nottest
 def test_cmd_line():
     cmd1 = dry_run_parse(['--job', './rabix/tests/test-cmdline/bwa-mem.yml#job'])
     tool1 = get_tool(cmd1)
@@ -69,11 +67,10 @@ def test_cmd_line():
     tool8 = get_tool(cmd8)
     assert tool8
 
-
 @nottest
 def test_expr_and_meta():
     sys.argv = ['rabix', '--job',
-                '/home/sinisa/devel/CWL/rabix/rabix/tests/test-expr/bwa-mem1.json',
+                './rabix/tests/test-expr/bwa-mem1.json#job',
                 '--dir', 'test1']
     main()
     with open(os.path.abspath('./test1') + '/output.sam.meta') as m:
@@ -81,7 +78,7 @@ def test_expr_and_meta():
         assert meta['expr_test'] == 'successful'
     shutil.rmtree(os.path.abspath('./test1'))
     sys.argv = ['rabix', '--job',
-                '/home/sinisa/devel/CWL/rabix/rabix/tests/test-expr/bwa-mem2.json',
+                './rabix/tests/test-expr/bwa-mem2.json#job',
                 '--dir', 'test2']
     main()
     with open(os.path.abspath('./test2') + '/output.sam.meta') as m:
@@ -92,7 +89,7 @@ def test_expr_and_meta():
 
 def test_fetch_remote_files():
     sys.argv = ['rabix', '--dir', 'testdir', '-t',
-                '/home/sinisa/devel/CWL/rabix/rabix/tests/test-cmdline/bwa-mem-tool.yml', '--', '--reads',
+                './rabix/tests/test-cmdline/bwa-mem-tool.yml#tool', '--', '--reads',
                 'https://s3.amazonaws.com/rabix/rabix-test/example_human_Illumina.pe_1.fastq', '--reads',
                 'https://s3.amazonaws.com/rabix/rabix-test/example_human_Illumina.pe_2.fastq', '--reference',
                 './rabix/tests/test-files/chr20.fa']
