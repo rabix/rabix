b6629ad1c6705a707f5896dbcee1a0f8571293f3
Peter Amstutz <peter.amstutz@curoverse.com>
2014-11-13 11:39:06 -0500

    Changes:  - Move 'stdin' from being a property of the adapter of a specific input to being part of the global tool adapter, for consistency with 'stdout'  - Add '--basedir' parameter and resolve relative paths in the input using 'basedir'.  - Add support for "$job" pointers into the job order, usable anywhere "$expr" is supported.  - 5 out of 6 conformance tests pass.

--- rabix/cliche/main.py
+++ rabix/cliche/main.py
@@ -13,13 +13,15 @@ def main():
     parser.add_argument("tool", type=str)
     parser.add_argument("job_order", type=str)
     parser.add_argument("--conformance-test", action="store_true")
+    parser.add_argument("--basedir", type=str)
+    parser.add_argument("--no-container", action="store_true")
     args = parser.parse_args()
 
     tool = from_url(args.tool)
     job = from_url(args.job_order)
     adapter = Adapter(tool)
     base_args = adapter._base_args(job)
-    args, stdin = adapter._arg_list_and_stdin(job)
+    args, stdin = adapter._arg_list_and_stdin(job, basedir=args.basedir)
     stdout = adapter._get_stdout_name(job)
     print(json.dumps({
         'args': map(six.text_type, base_args + args),
