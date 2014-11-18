d0ab0f0f6c17aa440d8d280f9b294c27bc68721c
Luka Stojanovic <luka.stojanovic@sbgenomics.com>
2014-11-18 18:50:44 +0100

    Workflow model and ExecutionGraph
    Next step should be Workflow runner that utilizes ExecutionGraph

--- rabix/workflows/__init__.py
+++ rabix/workflows/__init__.py
@@ -2,6 +2,7 @@
 CONFIG = {
     "runners": {
         "docker": "rabix.executors.runner.DockerRunners",
-        "native": "rabix.executors.runner.NativeRunner"
+        "native": "rabix.executors.runner.NativeRunner",
+        "Script": "rabix.executors.runner.ExpressionRunner"
     }
 }
