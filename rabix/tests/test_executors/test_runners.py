5d878c0289cf12d6c95ccbc0ac9d305bde58e32c
Luka Stojanovic <luka.stojanovic@sbgenomics.com>
2014-11-24 19:11:43 +0100

    Conformance test

--- rabix/tests/test_executors/test_runners.py
+++ rabix/tests/test_executors/test_runners.py
@@ -1,6 +1,7 @@
 import logging
+import six
 
-from nose.tools import nottest, raises
+from nose.tools import nottest, raises, assert_equal
 
 from os.path import abspath, join
 from rabix.common.ref_resolver import from_url
@@ -9,15 +10,21 @@ from rabix.executors.runner import *
 
 logging.basicConfig(level=logging.DEBUG)
 
+@nottest
+def assert_execution(job, outputs):
+    workflow = job['app']
+
+    result = run(workflow, job)
+    assert_equal(result, outputs)
+
 
 def test_workflow():
-
-    path = abspath(join(__file__, '../../test_runtime/workflow.yml'))
+    path = abspath(join(__file__, '../../test_runtime/wf_tests.yaml'))
 
     doc = from_url(path)
-    workflow = doc['workflows']['add_one_mul_two']
-    job = doc['jobs']['batch_add_one_mul_two']
-    print(workflow)
-    print(job)
-    result = run(workflow, job)
-    print(result)
+    tests = doc['tests']
+    print(tests)
+    for test_name, test in six.iteritems(tests):
+        if not test.get('requiresFeatures'):
+            print("Running test: " + test_name)
+            assert_execution(test['job'], test['outputs'])
