fa8bb853f75cf11f867df9b87b588edec83e7560
Luka Stojanovic <luka.stojanovic@sbgenomics.com>
2015-07-17 15:56:46 +0200

    minor test fix

--- rabix/tests/test_schema.py
+++ rabix/tests/test_schema.py
@@ -15,7 +15,7 @@ def test_simple_avro_schema():
             "type": "int"
         }]
     }
-    cons = make_avro(schema, [])
+    cons = make_avro(schema, []).schemas[0]
     a = next(iter(cons.fields))
     assert_equal(a.name, 'a')
     assert_equal(a.type.name, 'int')
