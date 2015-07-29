b254cc7810746a81da1b6aec481086254435c2aa
Luka Stojanovic <luka.stojanovic@sbgenomics.com>
2015-07-29 19:41:39 +0200

    Convert list of files when schema is File

--- rabix/common/models.py
+++ rabix/common/models.py
@@ -23,7 +23,7 @@ else:
 
 
 from rabix.common.errors import ValidationError, RabixError
-from rabix.common.util import wrap_in_list
+from rabix.common.util import wrap_in_list, map_or_apply
 
 
 log = logging.getLogger(__name__)
@@ -55,7 +55,7 @@ def construct_files(val, schema):
 
     if schema.type == 'record':
         if schema.name == 'File':
-            return File(val) if val else val
+            return map_or_apply(File, val) if val else val
         else:
             ret = {}
             for fld in schema.fields:
